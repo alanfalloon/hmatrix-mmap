0b37a159db9fd92bef241eac19452b55441f4a25
Alan Falloon <alan.falloon@gmail.com>
2011-01-25 19:55:11 -0500

    Enhance the documentation
    Add a description of memory-mapping in general including a small sales
    pitch and the usual warnings about portability. Add some sections and
    other clean-up to the generated documentation.

--- Data/Packed/Vector/MMap.hs
+++ Data/Packed/Vector/MMap.hs
@@ -1,6 +1,38 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+
+-- | Functions to represent a 'Vector' on disk in efficient, if
+-- unportable, ways.
+--
+-- This module uses memory-mapping, a feature of all modern
+-- operating-systems, to mirror the disk contents in memory. There are
+-- quite a few advantages to memory-mapping files instead of reading
+-- the files traditionally:
+--
+--  * Speed: memory-mapping is often much faster than traditional
+--    reading.
+--
+--  * Memory efficiency: Memory-mapped files are loaded into RAM
+--    on-demand, and easily swapped out. The upside is that the
+--    program can work with data-sets larger than the available RAM,
+--    as long as they are accessed carefully.
+--
+-- The caveat to using memory-mapping is that it makes the files
+-- specific to the current architecture because of the endianness of
+-- the data. For more information, see the description in
+-- "System.IO.MMap"
+--
+-- If you wish to write the contents in a portable fashion, either use
+-- the ASCII load and save functions in "Numeric.Container", or use
+-- the binary serialization in "Data.Binary".
+
 module Data.Packed.Vector.MMap (
+  -- * Memory-mapping 'Vector' from disk
   unsafeMMapVector,
+
+  -- * Writing 'Vector' to disk
+
+  -- | These functions write the 'Vector' in a way suitable for
+  -- reading back with 'unsafeMMapVector'.
   hPutVector,
   writeVector
 ) where
@@ -16,7 +48,14 @@ import qualified Data.Packed.Development as I
 import qualified Data.Packed.Vector as I
 import Data.Int
 
--- | Map a file into memory ('ReadOnly' mode) as an immutable vector.
+---------------------------
+-- Memory-Mapping 'Vector' from disk
+
+-- | Map a file into memory (read-only) as a 'Vector'.
+--
+-- It is considered unsafe because changes to the underlying file may
+-- (or may not) be reflected in the 'Vector', which breaks referential
+-- transparency.
 unsafeMMapVector :: forall a. Storable a => FilePath -- ^ Path of the file to map
                                          -> Maybe (Int64, Int) -- ^ 'Nothing' to map entire file into memory, otherwise 'Just (fileOffset, elementCount)'
                                          -> IO (I.Vector a)
@@ -26,6 +65,9 @@ unsafeMMapVector path range =
           Nothing -> Nothing
           Just (start, length) -> Just (start, length * sizeOf (undefined :: a))
      return $ I.unsafeFromForeignPtr foreignPtr offset (size `div` sizeOf (undefined :: a))
+
+---------------------------
+-- Writing 'Vector' to disk
 
 -- | Write out a vector verbatim into an open file handle.
 hPutVector :: forall a. Storable a => Handle -> I.Vector a -> IO ()
